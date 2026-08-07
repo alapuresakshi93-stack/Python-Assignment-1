1656f36df2dd0831dcddda95000a866d8ca7e150
alapuresakshi93-stack <alapuresakshi93@gmail.com>
2026-08-07 10:32:19 +0530

    Fix output display for modulus calculation

--- Assignment-1 hardcalculation.py
+++ Assignment-1 hardcalculation.py
@@ -1,13 +1,9 @@
-# Input two numbers
+
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 
-# Exponentiation
 power = a ** b
-
-# Modulus
 remainder = a % b
 
-# Display results
 print("Exponentiation (a^b):", power)
 print("Modulus (a % b):", remainder)
